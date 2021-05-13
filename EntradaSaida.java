915f9df6e55bc64d88a4f4469b6967eee2130b6b
orsolon <56239735+orsolon@users.noreply.github.com>
2021-05-12 21:15:53 -0300

    exercicio entrada/saida

---
+++ EntradaSaida.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class EntradaSaida {
+
+    
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            while(true) { //loop infinito
+                System.out.println("Digite um numero de 1 a 10");
+                int numero = input.nextInt();
+                System.out.println("Você digitou: " + numero);
+                System.out.println("Quer continuar? S/N");
+                String resposta = input.next();
+                if (resposta.equals("N")){
+                    break;
+                }
+
+            }
+
+
+
+    }
+}
