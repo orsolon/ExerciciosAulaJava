e02e41c6ec1408a9ccee25b85543e050c2796966
orsolon <56239735+orsolon@users.noreply.github.com>
2021-05-12 21:43:15 -0300

    coutEvens

---
+++ Even.java
@@ -0,0 +1,22 @@
+public class Even {
+
+    public static void main(String[] args) {
+        //Dado dois numeros exiba pares entre eles e quantos são os pares
+        int currentNumber, startNumber = 1, endNumber = 20, countEvens=0;
+        currentNumber = startNumber;
+
+        while(currentNumber < endNumber) {
+            if(isEvenNumber(currentNumber)){
+                System.out.println(currentNumber++);
+                countEvens++;
+            }
+            currentNumber++;
+        }
+
+        System.out.printf("there are %d between %d and %d", countEvens, startNumber, endNumber);
+    }
+
+    private static boolean isEvenNumber(int number){
+        return number % 2 == 0;
+    }
+}
