0b4bc9c28398d31a3085d999a4cf58295ea38d2b
orsolon <56239735+orsolon@users.noreply.github.com>
2021-05-14 01:32:52 -0300

    exercio tamanho string

--- Array4.java
+++ Array4.java
@@ -3,32 +3,23 @@ import java.util.Scanner;
 public class Array4 {
     public static void main(String[] args) {
         // Escreva um programa que lê 5 números e informa o maior, o menor e a média deles.
-        Scanner scan = new Scanner(System.in);
-        int n; 
+        int[] myNum = {10, 20, 30, 40, 3};
         int maior = 0;
         int menor = 0;
-        int media = (maior + menor)/2;
-
-        for (int i=0; i<5; i=i+1){
-            System.out.println("Digite um numero:");
-            n = scan.nextInt();
-
-            if(i==0){
-                maior = n;
-                menor = n;
-            }
-       
+        
+        for (int i=0; i<5; i++){
+            int n = myNum[i];
             if(n>maior){
                 maior = n;
             }  
-            if(n<menor){
+            if(n<maior){
                 menor = n;
             }
-
         }
-        System.out.println("Maior: " + maior);
+
+        float media = ((float)maior+menor)/2;
+        System.out.println("Maior; " + maior);
         System.out.println("Menor: " + menor);
         System.out.println("Media: " + media);
-        scan.close();
     }
 }
--- Array5.java
+++ Array5.java
@@ -1,19 +1,24 @@
 public class Array5 {
+    //Escreva um programa que lê o nome de 5 pessoas e armazena numa lista de convidados. 
+    //Ao final, informe qual o nome mais longo presente na lista.
     public static void main(String[] args) {
-        // Escreva um programa que lê 5 números e informa o maior, o menor e a média deles.
-        int[] myNum = {10, 20, 30, 40, 3};
+        String[] convidados = {"João", "Lucas", "Marcela", "Lais", "Tati"};
         int maior = 0;
-        int menor = 0;
-        for (int i=0; i<5; i=i+1){
-            int n = myNum[i];
-            if(n>maior){
-                maior = n;
-            }  
-            if(n<menor){
-                menor = n;
+        int indicepalavra=0;
+        int tamanho = convidados.length;
+
+        for (int i = 0; i < tamanho; i++) {
+            if (maior < convidados[i].length()) {
+                maior = convidados[i].length();
+                indicepalavra = i;
             }
         }
-        System.out.println("Maior" + maior);
-        System.out.println("Menor" + menor);
+        System.out.println(convidados[indicepalavra]);
     }
+
 }
+
+/*
+1 - inicializei a variavel maior com zero
+2 - criei um If onde ele analisa se a variavel maior é MENOR que o tamanho da palavra corrente, caso seja, maior irá receber o tamanho da palavra corrente e indicepalavra receberá o indice que indica a posição da palavra no "vetor".
+3 - por fim como sei aonde a maior palavra esta posicionada, utilizo a variavel indicepalavra para indicar qual é a maior.*/
